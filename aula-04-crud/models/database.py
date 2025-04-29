6644fd5233abfc586037a05fbaef5ea04d983bd5
mtLeme07 <mt.leme07@gmail.com>
2025-04-29 10:49:07 -0300

    Adicionanod BD

---
+++ aula-04-crud/models/database.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Carregando o SQLAlchemy na variavel DB
+db = SQLAlchemy();
+
+# Criando o Model (Orientado a Objeto, Classe)
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(150))
+    ano = db.Column(db.Integer)
+    categoria = db.Column(db.String(150))
+    plataforma = db.Column(db.String(150))
+    preco = db.Column(db.Float)
+    quantidade = db.Column(db.Integer)
+    
+    #Método construtor da classe
+    def __init__(self, titulo, ano, categoria, plataforma, preco, quantidade):
+        self.titulo = titulo
+        self.ano = ano
+        self.categoria = categoria
+        self.plataforma = plataforma
+        self.preco = preco
+        self.quantidade = quantidade
+    
+    
